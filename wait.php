a16fc35bc4f0865e0d1259e9afe8b4d42f53bcbc
SasakiRyota <ceruonxi@gmail.com>
2016-05-18 11:37:57 +0900

    Started

---
+++ wait.php
@@ -0,0 +1,53 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>LILIQ Develop - Waiting..</title>
+<link rel="stylesheet" type="text/css" href="screen.css">
+</head>
+
+<body>
+<img id="liliq" src="img/logo.png" alt="LILIQ">
+
+  <div id="screen_green">
+    <div id="screen_box">
+
+
+      <div id="screen_captcha">
+
+        <div id="face_box">　</div>
+
+        <div id="text_box">
+          <div id="ck">　</div>
+          <h1 id="status">出席</h1>
+          <p class="closer">＊秒後にクローズ</p><br clear="all">
+        </div>
+          <br clear="all">
+
+          <p id="name"  class="marginner">0X00000 藍井　上尾</p>
+          <div id="select_box">
+            <div id="choice_btn">
+              <p>　これは私ではありません（再認証）</p>
+            </div>
+          </div>
+
+      </div>
+
+
+    </div>
+  </div>
+
+
+  <div id="status_box">
+    <p class="clock_txt">00/00(*) 00:00</p>
+  </div>
+
+</body>
+</html>
+
+
+<?php
+?>
